7294395e1a03c1921d9c92c9041e20bf6d3eeb73
Cheng Chang <cheng@orby.ai>
2025-06-03 12:09:23 -0700

    Update eval_qwen_2_5_vl_subtask.sh

--- orby/scripts/eval_qwen_2_5_vl_subtask.sh
+++ orby/scripts/eval_qwen_2_5_vl_subtask.sh
@@ -1,5 +1,5 @@
 MODEL_PATH=Qwen/Qwen2.5-VL-7B-Instruct
-DATA_PATH=~/data/subtask_direct_distill/mix/test/executor.parquet
+DATA_PATH=~/data/subtask_direct_distill/mix/test/combined.parquet
 REWARD_FILE=orby/reward/subtask.py
 REWARD_FN=reward_func
 OUTPUT_FILE=test-output-subtask-1.parquet
