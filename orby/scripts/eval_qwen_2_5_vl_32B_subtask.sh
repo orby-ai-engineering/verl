0dbec3fae1aab82928fdb69d48a74bca4d136b50
Cheng Chang <cheng@orby.ai>
2025-06-11 02:22:08 -0700

    patch reward_kwargs for subtask rule based reward scripts

--- orby/scripts/eval_qwen_2_5_vl_32B_subtask.sh
+++ orby/scripts/eval_qwen_2_5_vl_32B_subtask.sh
@@ -34,6 +34,6 @@ python3 -m orby.trainer.main_eval \
     data.response_key=responses \
     custom_reward_function.path=$REWARD_FILE \
     custom_reward_function.name=$REWARD_FN \
-    +custom_reward_function.coordinates_metric=$COORDINATES_METRIC \
-    +custom_reward_function.coordinates_gaussian_sigma=$COORDINATES_GAUSSIAN_SIGMA \
-    +custom_reward_function.coordinates_pixel_square_size=$COORDINATES_PIXEL_SQUARE_SIZE
+    +custom_reward_function.reward_kwargs.coordinates_metric=$COORDINATES_METRIC \
+    +custom_reward_function.reward_kwargs.coordinates_gaussian_sigma=$COORDINATES_GAUSSIAN_SIGMA \
+    +custom_reward_function.reward_kwargs.coordinates_pixel_square_size=$COORDINATES_PIXEL_SQUARE_SIZE
