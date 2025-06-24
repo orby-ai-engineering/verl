ff56c36e5930bd673e9a352e531904e921d86a66
Rishu <rishu@orby.ai>
2025-06-24 13:35:34 -0700

    refactoring subtask and sft (#26)
    * refactoring subtask and sft into different pipelines

--- orby/data/convert_screenspot_v2.py
+++ orby/data/convert_screenspot_v2.py
@@ -35,7 +35,7 @@ from qwen_agent.llm.fncall_prompts.nous_fncall_prompt import (
 from orby.utils.dataset.qwen_agent_function_call import ComputerUse
 
 from verl.utils.hdfs_io import copy, makedirs
-from orby.data.prompts import get_sft_messages
+from orby.data.prompts import get_subtask_messages
 
 MODEL_PATH = "Qwen/Qwen2.5-VL-7B-Instruct"
 PROCESSOR = AutoProcessor.from_pretrained(MODEL_PATH, use_fast=True)
@@ -151,7 +151,14 @@ def process_json_file(json_path, image_dir, split, prompt_format="thinking"):
                 },
             ]
         elif prompt_format == "sft":
-            prompt, _ = get_sft_messages(example["instruction"], None, None)
+            data["prompt"] = [
+                {
+                    "role": "user",
+                    "content": ("<image> Instruction: " + example["instruction"]),
+                },
+            ]
+        elif prompt_format == "subtask":
+            prompt = get_subtask_messages(example["instruction"])
             data["prompt"] = prompt
             
         elif prompt_format == "qwen":  # qwen format
@@ -199,9 +206,9 @@ if __name__ == "__main__":
     )
     parser.add_argument(
         "--prompt_format",
-        choices=["thinking", "qwen", "sft"],
+        choices=["thinking", "qwen", "subtask", "sft"],
         default="thinking",
-        help="Select prompt format: ['thinking', 'qwen', 'sft']",
+        help="Select prompt format: ['thinking', 'qwen', 'subtask', 'sft']",
     )
 
     args = parser.parse_args()
