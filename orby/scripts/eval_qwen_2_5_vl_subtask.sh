955e9e50e9ee2f1c3a72101e96bbfdfeb7aa961a
Cheng Chang <cheng@orby.ai>
2025-06-25 17:26:37 -0700

    try main eval with score save

--- orby/scripts/eval_qwen_2_5_vl_subtask.sh
+++ orby/scripts/eval_qwen_2_5_vl_subtask.sh
@@ -32,6 +32,7 @@ python3 -m orby.trainer.main_eval \
     data.path=$OUTPUT_FILE \
     data.prompt_key=prompt \
     data.response_key=responses \
+    +data.save_scores=True \
     custom_reward_function.path=$REWARD_FILE \
     custom_reward_function.name=$REWARD_FN \
     +custom_reward_function.reward_kwargs.coordinates_metric=$COORDINATES_METRIC \
