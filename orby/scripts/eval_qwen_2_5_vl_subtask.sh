6c840c368fdc3d72cc3e56dbf6212af85b6c0d2f
Cheng Chang <cheng@orby.ai>
2025-06-27 10:46:30 -0700

    Update eval_qwen_2_5_vl_subtask.sh

--- orby/scripts/eval_qwen_2_5_vl_subtask.sh
+++ orby/scripts/eval_qwen_2_5_vl_subtask.sh
@@ -31,7 +31,7 @@ python3 -m orby.trainer.main_generation \
 python3 -m orby.trainer.main_eval \
     data.path=$OUTPUT_FILE \
     data.prompt_key=prompt \
-    data.response_key=prediction \
+    data.response_key=responses \
     +data.save_scores=True \
     custom_reward_function.path=$REWARD_FILE \
     custom_reward_function.name=$REWARD_FN \
