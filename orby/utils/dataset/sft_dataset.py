38f98a4efce9f6b64e19f72cd3088fdce8428b17
Rishu <rishu@orby.ai>
2025-06-27 17:15:47 -0700

    Enhaced loss mask debugging and response_key check and changed ability of uground (#33)
    * minor fixes to response_key logic
    
    * changed ability of uground

--- orby/utils/dataset/sft_dataset.py
+++ orby/utils/dataset/sft_dataset.py
@@ -162,7 +162,7 @@ class SFTDataset(Dataset):
     def _build_messages(self, example: dict):
         messages: list = example.pop(self.prompt_key)
         # SFT Training will always assume that the response_key is 'response' (which is different from verl which assumes that it is in the extra_info dict under the key 'answer')
-        if self.response_key is not "response" or self.response_key not in example:
+        if (self.response_key != "response") or (self.response_key not in example):
             raise ValueError(f"response_key '{self.response_key}' is not set to 'response' or is not present in example. Available keys: {list(example.keys())}")
         
         response_messages = ""
