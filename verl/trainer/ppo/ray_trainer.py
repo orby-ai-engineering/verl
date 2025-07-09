b388a78feea7341010fb3aa373a4a2bce015bcab
Gang Li <ligang@orby.ai>
2025-07-08 23:28:10 -0700

    Gaurd by flag

--- verl/trainer/ppo/ray_trainer.py
+++ verl/trainer/ppo/ray_trainer.py
@@ -952,10 +952,11 @@ class RayPPOTrainer:
                     batch.non_tensor_batch["uid"] = np.array([str(uuid.uuid4()) for _ in range(len(batch.batch))], dtype=object)
 
                     # Add back multi_modal_inputs to batch.non_tensor_batch.
-                    if "multi_modal_inputs" in gen_batch.non_tensor_batch:
-                        batch.non_tensor_batch["multi_modal_inputs"] = gen_batch.non_tensor_batch["multi_modal_inputs"]
-                    if "multi_modal_data" in gen_batch.non_tensor_batch:
-                        batch.non_tensor_batch["multi_modal_data"] = gen_batch.non_tensor_batch["multi_modal_data"]
+                    if self.config.actor_rollout_ref.rollout.get("remove_multimodal_data_from_rollout", True):
+                        if "multi_modal_inputs" in gen_batch.non_tensor_batch:
+                            batch.non_tensor_batch["multi_modal_inputs"] = gen_batch.non_tensor_batch["multi_modal_inputs"]
+                        if "multi_modal_data" in gen_batch.non_tensor_batch:
+                            batch.non_tensor_batch["multi_modal_data"] = gen_batch.non_tensor_batch["multi_modal_data"]
 
                     # repeat to align with repeated responses in rollout
                     batch = batch.repeat(repeat_times=self.config.actor_rollout_ref.rollout.n, interleave=True)
