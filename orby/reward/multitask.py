704f5c0e526ff8143e2dd4a8c22a9d9ad1094359
Gang Li <ligang@orby.ai>
2025-07-25 17:09:42 -0700

    Fix multitask reward fn

--- orby/reward/multitask.py
+++ orby/reward/multitask.py
@@ -10,12 +10,25 @@ def training_reward_func(
     if data_source in ["uground"]:
         from orby.reward import uground
 
-        return uground.reward_func(data_source, solution_str, ground_truth, **kwargs)
-    elif data_source in ["subtask"]:
+        return uground.reward_func(
+            data_source,
+            solution_str,
+            ground_truth,
+            kwargs.get("prompt_format"),
+            kwargs.get("use_gaussian"),
+            kwargs.get("extra_info"),
+        )
+    elif data_source in ["subtask_direct_distill"]:
         from orby.reward import subtask
 
         return subtask.training_reward_func(
-            data_source, solution_str, ground_truth, **kwargs
+            data_source,
+            solution_str,
+            ground_truth,
+            kwargs.get("coordinates_metric", "gaussian"),
+            kwargs.get("coordinates_gaussian_sigma", 5),
+            kwargs.get("coordinates_pixel_square_size", 10),
+            kwargs.get("extra_info"),
         )
     else:
         raise NotImplementedError
@@ -30,12 +43,25 @@ def eval_reward_func(
     if data_source in ["uground"]:
         from orby.reward import uground
 
-        return uground.reward_func(data_source, solution_str, ground_truth, **kwargs)
-    elif data_source in ["subtask"]:
+        return uground.reward_func(
+            data_source,
+            solution_str,
+            ground_truth,
+            kwargs.get("prompt_format"),
+            kwargs.get("use_gaussian"),
+            kwargs.get("extra_info"),
+        )
+    elif data_source in ["subtask_direct_distill"]:
         from orby.reward import subtask
 
         return subtask.eval_reward_func(
-            data_source, solution_str, ground_truth, **kwargs
+            data_source,
+            solution_str,
+            ground_truth,
+            kwargs.get("coordinates_metric", "gaussian"),
+            kwargs.get("coordinates_gaussian_sigma", 5),
+            kwargs.get("coordinates_pixel_square_size", 10),
+            kwargs.get("extra_info"),
         )
     else:
         raise NotImplementedError
